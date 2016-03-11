f99ca0b7c67586f6b453d00505c339ae53833eab
Matthew McClure <matthewmcclure@Matthews-MacBook-Pro.local>
2016-03-11 14:20:27 -0600

    add proj

---
+++ main.js
@@ -0,0 +1,21 @@
+$( document ).ready(function() {
+
+  var width = $(window).width();
+
+  function makeBigLayout(){
+    $('.max-width-container').css({
+      'max-width': '1400px',
+      'height': '760px'
+    });
+    $('.shirt-panel').css({
+      // 'height': '700px'
+    });
+  }
+
+  if (width > 1300){
+    makeBigLayout();
+
+  }
+
+
+});
